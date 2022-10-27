86ff12c5625b916a650d79f778c5bb66481ab197
Erwan de Belleville <edebelleville@hotmail.fr>
2022-10-27 14:03:26 +0200

    modifs

--- controllers/sauce.controllers.js
+++ controllers/sauce.controllers.js
@@ -21,7 +21,7 @@ exports.getOneSauce = (req, res) => {
     .catch((error) => res.status(500).json({error: "internal server error"}));
 };
 
-// CREATE
+// CREATE A SAUCE 
 exports.createSauce = (req, res) => {
   const sauceObject = JSON.parse(req.body.sauce);
   delete sauceObject._id;
@@ -37,8 +37,10 @@ exports.createSauce = (req, res) => {
     });
 };
 
-// MODIFY
+// MODIFY A SAUCE
 exports.modifySauce = (req, res) => {
+
+  // Verification ID 
   const modifiedSauce = req.file ? {
     ...JSON.parse(req.body.sauce),
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
@@ -48,46 +50,17 @@ exports.modifySauce = (req, res) => {
   .then(() => res.status(200).json({ message: 'Objet modifié !'}))
   .catch(error => res.status(500).json({error: "internal server error"}));
   
-  /*
-  delete modifiedSauce._userId;
-  Sauce.findOne({_id: req.params.id})
-      .then((element) => {
-          if (element.userId != req.auth.userId) {
-              res.status(401).json({ message : 'Not authorized'});
-          } else {
-              Sauce.updateOne({ _id: req.params.id}, { ...modifiedSauce, _id: req.params.id})
-              .then(() => res.status(200).json({message : 'Objet modifié!'}))
-              .catch(error => res.status(500).json({ error: "internal server error" }));
-          }
-      })
-      .catch((error) => {
-          res.status(400).json({ error });
-      });
-*/
-  
 };
 
 
-// DELETE
-/*
-exports.deleteSauce = (req, res) => {  
-  Sauce.deleteOne({ _id: req.params.id })
-    .then((sauce) =>{
-      console.log(sauce.imageUrl);
-      const filename = sauce.imageUrl.split("/images/")[1];
-      console.log("test",filename);
-      fs.unlink(`images/${filename}`, () => {
-        res.status(200).json({ message: "La sauce est bien supprimée" })
-        console.log('oui')
-      })
-  })
-  .catch(error => res.status(500).json({error: "internal server error" }) )
-};
-*/
-
+// DELETE A SAUCE 
 exports.deleteSauce = (req, res) => {
+  // Verifier user ID 
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
+      if(!sauce){
+        res.status(500).json({error: "internal server error" })
+      }
       const filename = sauce.imageUrl.split("/images/")[1];
       fs.unlink(`images/${filename}`, () => {
         Sauce.deleteOne({ _id: req.params.id })
@@ -101,10 +74,147 @@ exports.deleteSauce = (req, res) => {
 };
  
 
-// LIKE & DISLIKE
+
+// LIKE & DISLIKE A SAUCE 
+
+// User a 3 actions possibles  
+// tableau d'ID et on récupère le total du tableau pour avoir le nombre de like
+
+
 exports.likeSauce = (req, res) => {
-  Sauce.findOne({ _id: req.params.id });
+  Sauce.findOne({ _id: req.params.id })
+  .then((sauce) =>{
+   
+    console.log("like", req.body.like);
+    const sauceLikes = sauce.usersLiked;
+    const sauceDislikes = sauce.usersDisliked;
+    console.log('before',sauceLikes);
+    console.log('before',sauceDislikes);
+
+    
+    //  LIKE
+    if(req.body.like === 1) {
+      // Si utilisateur existe pas dans les 2 tableaux :
+      if (!sauceLikes.includes(req.userId) && !sauceDislikes.includes(req.userId)){
+          console.log('LIKE CONDITIONS')
+          sauceLikes.push(req.userId);
+          sauce.likes++;
+        }else {
+          res.status((500)).json({error: "internal server error" });
+        }
+    }
+
+      // DISLIKE
+      if(req.body.like === -1) {
+        if (!sauceLikes.includes(req.userId) && !sauceDislikes.includes(req.userId)){
+            console.log('DISLIKE CONDITIONS')
+            sauceDislikes.push(req.userId);
+            sauce.dislikes++;
+          } else {
+            res.status(500).json({error: "internal server error" });
+          }
+      }
+
+    // NO LIKE -- INITIAL STATE
+    if (req.body.like === 0) {
+      console.log('NO LIKE CONDITIONS')
+
+      if(sauceLikes.includes(req.userId)) {
+        let indexUser = sauceLikes.indexOf(req.userId);
+        sauceLikes.splice(indexUser, 1);
+        sauce.likes--;
+        console.log('no like');
+      } else {
+          res.status(500).json({error: "internal server error" });
+      }
+
+      if(sauceDislikes.includes(req.userId)) {
+          let indexUser = sauceDislikes.indexOf(req.userId);
+          sauceDislikes.splice(indexUser, 1);
+          sauce.dislikes--;
+          console.log('no like');
+        } else {
+          res.status(500).json({error: "internal server error" });
+      }
+      
+    
+    }
+    
+    console.log('after',sauceLikes);
+    console.log('after',sauceDislikes);
+
+    sauce.save();
+    res.status(200).json({ message: 'sauce like' });
+
+  })
+  .catch((error) =>res.status(500).json({error: "internal server error" }) )
+
+
+  /*
+  const sauce = Sauce.findOne({ _id: req.params.id });
+  const sauceLikes = sauce.usersLiked;
+  const sauceDislikes = sauce.usersDisliked;
+
+    console.log(sauce);
+
+    // LIKE
+    if (req.body.like === 1) {
+      if (!sauceLikes.includes(req.userId)) {
+        if (!sauceDislikes.includes(req.userId)) {
+          sauceLikes.push(req.userId);
+          sauce.likes++;
+          console.log('like');
+        } else {
+          res.status((500)).json({error: "internal server error" });
+        }
+      } else {
+        res.status(500).json({error: "internal server error" });
+      }
+    }
+
+    // DISLIKE
+    if (req.body.like === -1) {
+      if (!sauceDislikes.includes(req.userId)) {
+        if (!sauceLikes.includes(req.userId)) {
+          sauceDislikes.push(req.userId);
+          sauce.dislikes++;
+          console.log('dislike')
+        } else {
+          res.status(400).json({error: "internal server error" });
+        }
+      } else {
+        res.status(400).json({error: "internal server error" });
+      }
+    }
 
 
 
+    // NO LIKE
+    if (req.body.like === 0) {
+
+      
+      if (sauceLikes.includes(req.userId)) {
+        let indexUser = sauceLikes.indexOf(req.userId);
+        sauceLikes.splice(indexUser, 1);
+        sauce.likes--;
+        console.log("no like")
+      } else {
+          res.status(500).json({error: "internal server error" });
+      }
+
+      if (sauceDislikes.includes(req.userId)) {
+          let indexUser = sauceDislikes.indexOf(req.userId);
+          sauceDislikes.splice(indexUser, 1);
+          sauce.dislikes--;
+          console.log('no like');
+        } else {
+          res.status(500).json({error: "internal server error" });
+      }
+
+
+    }
+
+    Sauce.save();
+    res.status(200).json({ message: 'sauce like' });
+    */
 };
---
+++ readme.md
@@ -0,0 +1,14 @@
+Pour faire fonctionner l'application:
+
+
+
+-------Pour lancer le serveur backend, depuis le repertoire de travail :
+
+Ouvrir un nouveau terminal, entrer la commande "cd piiquante-api", puis "npm install" (pour installer les fichiers nécessaires), 
+
+puis entrer la commande "npm start" pour lancer le projet.
+
+---------pour lancer l'appli frontend, depuis le reprtoire de travail :
+
+Ouvrir un nouveau terminal, entrer la commande "cd front", puis "npm install" (pour installer les fichiers nécessaires), 
+puis entrer la commande "npm start".
